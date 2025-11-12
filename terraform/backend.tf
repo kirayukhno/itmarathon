875791a7229a0171148a638c4e6d8bf773c21275
Kira <kira.yukhno123@gmail.com>
2025-11-12 10:18:31 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "itmarathon-bucket-kira"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
